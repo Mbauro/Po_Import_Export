715f62ee46ea3b4197218f003a07e50b0c5fffcc
m.labruna <m.labruna@reply.it>
2022-07-27 12:36:49 +0200

    Updated main.go: Start importer if args passed to the program are > 2

--- cmd/PoImportExport/main.go
+++ cmd/PoImportExport/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Mbauro/Po_Import_Export/internal/exporter"
+	"github.com/Mbauro/Po_Import_Export/internal/importer"
 	"log"
 	"os"
 )
 
 func main() {
-	filePath := os.Args[1]
-
 	if len(os.Args) == 2 {
 
+		filePath := os.Args[1]
 		err := exporter.ExportPoFileToCsv(filePath)
 
 		if err != nil {
@@ -19,7 +18,13 @@ func main() {
 		}
 
 	} else if len(os.Args) > 2 {
+		importFilePath := os.Args[1]
+		poFilePath := os.Args[2]
 
-		fmt.Println("importer")
+		err := importer.ImportFileToPo(importFilePath, poFilePath)
+
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
