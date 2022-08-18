cd631e6c1cac3e975776ddcb08bbab2009b062d5
m.labruna <m.labruna@reply.it>
2022-08-18 15:38:54 +0200

    Updated importer.go: Is the context (Strategy pattern) which implements the method to start the import workflow of the translation file

--- internal/importer/importer.go
+++ internal/importer/importer.go
@@ -1,5 +1,29 @@
 package importer
 
-func ImportFileToPo(importFilePath string, poFilePath string) error {
-	return ImportCsvFileToPo(importFilePath, poFilePath)
+type Importer struct {
+	fileFormat     FileFormat
+	importFilepath string
+	poFilepath     string
+}
+
+func (i *Importer) SetFileFormat(fileFormat FileFormat) {
+	i.fileFormat = fileFormat
+}
+
+func (i *Importer) SetImportFilePath(filepath string) {
+	i.importFilepath = filepath
+}
+
+func (i *Importer) SetPoFilepath(filepath string) {
+	i.poFilepath = filepath
+}
+
+func (i *Importer) ImportFile() error {
+	err := i.fileFormat.importFile(i.importFilepath, i.poFilepath)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
